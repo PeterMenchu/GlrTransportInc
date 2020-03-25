6b8c3a281622fab0591899ed2d11e7766575b67d
Peter Menchu <menchupeter2@gmail.com>
2020-03-25 06:59:14 -0500

    Added the models for freight bill and users

---
+++ GlrTransportInc/Models/FreightBill.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace GlrTransportInc.Models
+{
+    public enum FbStatus
+    {
+        FreightPending,
+        FreightCompleted,
+        FreightCanceled
+    }
+
+    public class FreightBill
+    {
+        public int ID { get; set; }
+        private string FreightBillNumber { get; set; }
+        public string Customer { get; set; }
+        public string Driver { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string FromLocation { get; set; }
+        public string PoNumber { get; set; }
+        public string TruckNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string ToZip { get; set; }
+        public string ToState { get; set; }
+        public string ToStreet { get; set; }
+        public string ToCity { get; set; }
+
+        public string ToLocation => $"{ToStreet} {ToCity} {ToState} {ToZip}";
+
+        public FbStatus Status { get; set; }
+        public string CurrentStatus;
+
+        public FreightBill()
+        {
+            this.Status = FbStatus.FreightPending;
+        }
+        
+    }
+}
---
+++ GlrTransportInc/Models/User.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace GlrTransportInc.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public int TypeOfEmployee { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public bool LoggedIn { get; set; }
+        public bool IsaFieldEmpty()
+        {
+            if (this.FirstName == null 
+                || this.LastName == null
+                || this.EmailAddress == null
+                || this.Password == null)
+            {
+                return true;
+            }return false;
+        }
+        
+        public User()
+        {
+            // this.FullName => $"{FirstName} {LastName}";
+            this.TypeOfEmployee = -1;
+        }
+    }
+}
