ba376f342f3fe74b537c5bf71bb8dad261c4c536
joe-niu <joeniu@my.unt.edu>
2020-04-06 23:33:59 -0500

    Initial implementation of Calendar

---
+++ GlrTransportInc/Pages/Calendar/Calendar.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GlrTransportInc.Data;
+
+
+namespace GlrTransportInc.Pages.Calendar
+{
+    public class CalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
