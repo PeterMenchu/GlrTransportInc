e6ed905489b8d5f25232a98de49e089a73715327
Jobert Cabuslay <jobertcabuslay@yahoo.com>
2020-04-07 13:31:40 -0500

    Update Pages/Calendar/Index.cshtml
    Updated to have IndexModel for backend connection eventually

--- GlrTransportInc/Pages/Calendar/Index.cshtml.cs
+++ GlrTransportInc/Pages/Calendar/Index.cshtml.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GlrTransportInc.Data;
+using GlrTransportInc.Models;
 
 namespace GlrTransportInc.Pages.Calendar
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        /*public void OnGet()
         {
+        }*/
+        private readonly GlrTransportInc.Data.ApplicationDbContext _context;
+
+        public IndexModel(GlrTransportInc.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<FreightBill> FreightBill { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            FreightBill = await _context.FreightBill.ToListAsync();
         }
     }
 }
