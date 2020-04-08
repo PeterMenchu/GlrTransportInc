e2e5e4e4be889f23ccb3ed947dd1cec774e1b00d
soyrodrigo01 <56319147+soyrodrigo01@users.noreply.github.com>
2020-04-07 23:54:39 -0500

    Added all the fields according to Gerry's sample freight bill.
    Also did the following: Created a new migration for the new freight bill model, added states to the different state drop down menus, made it so that dates can now be inputted with a calendar.

--- GlrTransportInc/Models/FreightBill.cs
+++ GlrTransportInc/Models/FreightBill.cs
@@ -13,23 +13,61 @@ namespace GlrTransportInc.Models
     public class FreightBill
     {
         public int ID { get; set; }
-        private string FreightBillNumber { get; set; }
-        public string Customer { get; set; }
-        public string Driver { get; set; }
+        public string Customer { get; set; } //ok
+        public string FreightBillNumber { get; set; } //ok
+        public string FromName { get; set; } //ok
+        public string FromStreet { get; set; } //ok
+        public string FromCity { get; set; } //ok
+        public string FromState { get; set; } //ok
+        public string FromZip { get; set; } //ok
+        public string ToName { get; set; } //ok
+        public string ToStreet { get; set; } //ok
+        public string ToCity { get; set; } //ok
+        public string ToState { get; set; } //ok
+        public string ToZip { get; set; } //ok
 
-        public DateTime Date { get; set; }
+        public DateTime ScheduledDate { get; set; } //ok
+        public string Size { get; set; } //ok
+        public string BranchAndDescription { get; set; } //ok
+        public string Unit { get; set; } //ok
+        public string PoNumber { get; set; } //ok
+        public string DocJob { get; set; } //ok
+        public string Comments { get; set; }
 
-        public string FromLocation { get; set; }
-        public string PoNumber { get; set; }
-        public string TruckNumber { get; set; }
-        public string PhoneNumber { get; set; }
-        public string EmailAddress { get; set; }
-        public string ToZip { get; set; }
-        public string ToState { get; set; }
-        public string ToStreet { get; set; }
-        public string ToCity { get; set; }
+        public string Labor1 { get; set; }
+        public string Labor2 { get; set; }
+        public string Labor3 { get; set; }
+        public string Labor4 { get; set; }
+        public string Labor5 { get; set; }
+        public string Labor6 { get; set; }
+        public string Labor7 { get; set; }
+        public string Labor8 { get; set; }
+        public string Labor9 { get; set; }
+        public float Cost1 { get; set; }
+        public float Cost2 { get; set; }
+        public float Cost3 { get; set; }
+        public float Cost4 { get; set; }
+        public float Cost5 { get; set; }
+        public float Cost6 { get; set; }
+        public float Cost7 { get; set; }
+        public float Cost8 { get; set; }
+        public float Cost9 { get; set; }
 
-        public string ToLocation => $"{ToStreet} {ToCity} {ToState} {ToZip}";
+        public float Total => Cost1 + Cost2 + Cost3 + Cost4 + Cost5 + Cost6 + Cost7 + Cost8 + Cost9; //ok
+
+
+        public string TruckNumber { get; set; } //ok
+        public string Driver { get; set; } // ok
+        public string ReceivedBy { get; set; } //ok
+        public DateTime CompletionDate { get; set; } //ok
+        public string SiteName { get; set; } //ok
+        public string SitePhoneNumber { get; set; } //ok
+        public DateTime DueDate { get; set; } //ok
+        public string EmailAddress { get; set; } // ok?
+
+
+        public string ToLocation => $"{ToStreet} {ToCity} {ToState} {ToZip}"; //ok
+        public string FromLocation => $"{FromStreet} {FromCity} {FromState} {FromZip}"; //ok
 
         public FbStatus Status { get; set; }
         public string CurrentStatus;
