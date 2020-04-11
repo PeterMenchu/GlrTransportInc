96a9f94c56712c5d384bf3f2e9e655396d1ee742
Peter Menchu <menchupeter2@gmail.com>
2020-04-10 22:55:45 -0500

    Added data grabbing from freight bill table to calendar, just customer name for now (BillName list holds this), when I find out proper format for date I'll do that. Renamed the Calendar/index to Calendar.cshtml+cshtml.cs as well as changed the model name to CalendarModel. Renamed map's IndexModel to MapModel to line up with the new name, and set the navbar so it links to the renamed calendar and map pages.

--- GlrTransportInc/Pages/Map/map.cshtml.cs
+++ GlrTransportInc/Pages/Map/map.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GlrTransportInc.Pages.Map
 {
-    public class IndexModel : PageModel
+    public class MapModel : PageModel
     {
         public void OnGet()
         {
