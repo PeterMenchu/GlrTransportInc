08c7d9fb3942042857aedb99397cb6bda47e4bd3
soyrodrigo01 <56319147+soyrodrigo01@users.noreply.github.com>
2020-04-13 23:00:03 -0500

    Tried adding a new field for the permits, will work on it later.

---
+++ GlrTransportInc/Data/Migrations/20200414023758_Permits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GlrTransportInc.Data.Migrations
+{
+    public partial class Permits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Permit",
+                table: "FreightBill",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Permit",
+                table: "FreightBill");
+        }
+    }
+}
