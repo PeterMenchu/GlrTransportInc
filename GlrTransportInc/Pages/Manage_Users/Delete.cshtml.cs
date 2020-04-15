54744a7f8f2e3fdaa5a86344cd36c194690346af
soyrodrigo01 <56319147+soyrodrigo01@users.noreply.github.com>
2020-04-14 23:55:34 -0500

    Created the user management page, implementing deleting users, added this to the Users drop down nav item. Fixed bug where the labor-cost table would mess up on mobile (add and edit freight bill).
    Still need to work on editing users, don't know what fields should be editable.

--- GlrTransportInc/Pages/Manage_Users/Delete.cshtml.cs
+++ GlrTransportInc/Pages/Manage_Users/Delete.cshtml.cs
@@ -2,15 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GlrTransportInc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GlrTransportInc.Pages.Manage_Users
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly GlrTransportInc.Data.ApplicationDbContext _context;
+
+        public DeleteModel(GlrTransportInc.Data.ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public UserModel UserModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            UserModel = await _context.UserModel.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            UserModel = await _context.UserModel.FindAsync(id);
+
+            if (UserModel != null)
+            {
+                _context.UserModel.Remove(UserModel);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
--- GlrTransportInc/Pages/Manage_Users/Index.cshtml.cs
+++ GlrTransportInc/Pages/Manage_Users/Index.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GlrTransportInc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GlrTransportInc.Pages.Manage_Users
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly GlrTransportInc.Data.ApplicationDbContext _context;
+
+        public IndexModel(GlrTransportInc.Data.ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<UserModel> UserModel { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            UserModel = await _context.UserModel.OrderBy(a => a.Name).ToListAsync();
         }
     }
 }
