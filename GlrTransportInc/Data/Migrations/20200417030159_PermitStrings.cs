0cc83fed879ec96c5227ebf81ae1b20762d59186
soyrodrigo01 <56319147+soyrodrigo01@users.noreply.github.com>
2020-04-16 23:30:30 -0500

    Tried to link uploaded files to the freight bill....

---
+++ GlrTransportInc/Data/Migrations/20200417030159_PermitStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GlrTransportInc.Data.Migrations
+{
+    public partial class PermitStrings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Permit",
+                table: "FreightBill",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Permit",
+                table: "FreightBill",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- GlrTransportInc/Pages/Freight_Bills/UploadPermit.cshtml.cs
+++ GlrTransportInc/Pages/Freight_Bills/UploadPermit.cshtml.cs
@@ -1,28 +1,88 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using GlrTransportInc.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GlrTransportInc.Pages.Freight_Bills
 {
     public class UploadPermitModel : PageModel
     {
         private IWebHostEnvironment _environment;
-        public UploadPermitModel(IWebHostEnvironment environment)
+        private readonly GlrTransportInc.Data.ApplicationDbContext _context;
+        public UploadPermitModel(IWebHostEnvironment environment, GlrTransportInc.Data.ApplicationDbContext context)
         {
             _environment = environment;
+            _context = context;
         }
         [BindProperty]
         public IFormFile Upload { get; set; }
-        public async Task OnPostAsync()
+        public FreightBill FreightBill { get; set; }
+        public IList<UserModel> UserModel { get; set; }
+        public static string Name;
+        public static string Position;
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            UserModel = await _context.UserModel.ToListAsync();
+            foreach (var item in UserModel)
+            {
+                if ((item.Email) == User.Identity.Name)
+                {
+                    Name = item.Name;
+                    Position = item.Position;
+                }
+            }
+            FreightBill = await _context.FreightBill.FirstOrDefaultAsync(m => m.ID == id);
+            if (FreightBill == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
             var file = Path.Combine(_environment.ContentRootPath, "wwwroot/permits", Upload.FileName);
             using (var fileStream = new FileStream(file, FileMode.Create))
             {
                 await Upload.CopyToAsync(fileStream);
             }
+            //Probably want to update it here <---------- I tried FreightBill.Permit = Upload.FileName; but it doesn't work.
+            _context.Attach(FreightBill).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FreightBillExists(FreightBill.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+        private bool FreightBillExists(int id)
+        {
+            return _context.FreightBill.Any(e => e.ID == id);
         }
     }
 }
