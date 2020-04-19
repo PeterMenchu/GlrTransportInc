84715437ec3f09b982abc535e2261479e6b117b5
Peter Menchu <menchupeter2@gmail.com>
2020-04-19 07:44:52 -0500

    Added user calendar, still need the other part of the sponsor's requirement regarding the main calendar. Fixed incorrect namespace reference in calendar model. Added email to contact page.

--- GlrTransportInc/Pages/Calendar/UserCalendar.cshtml.cs
+++ GlrTransportInc/Pages/Calendar/UserCalendar.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,17 +10,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GlrTransportInc.Pages.Calendar
 {
-    public class CalendarModel : PageModel
+    public class UserCalendarModel : PageModel
     {
         // variables for the calendar 
         public static List<int> Id = new List<int>();
         public static List<string> BillName = new List<string>();
         public static List<DateTime> StartDate = new List<DateTime>();
-        //public static List<DateTime> DueDate = new List<DateTime>();
         public static List<FbStatus> Status = new List<FbStatus>();
+        public IList<UserModel> Users { get; set; }
+
+        public static string DriverName;
         // _context is for grabbing bill data
         private readonly GlrTransportInc.Data.ApplicationDbContext _context;
-        public CalendarModel(GlrTransportInc.Data.ApplicationDbContext context)
+        public UserCalendarModel(GlrTransportInc.Data.ApplicationDbContext context)
         {
             _context = context;
         }
@@ -29,7 +31,7 @@ namespace GlrTransportInc.Pages.Calendar
         // OnGetAsync is called when page is loaded, sets needed values from freight table
         public async Task OnGetAsync()
         {
-            //Data Cleared so data isn't kept after each call
+            //Data Cleared so data isn't kept after each call (each item kept getting added on each get call for this page)
             Id.Clear();
             BillName.Clear();
             StartDate.Clear();
@@ -38,14 +40,28 @@ namespace GlrTransportInc.Pages.Calendar
 
             // enable data retrieval from DB to bill model
             FreightBill = await _context.FreightBill.ToListAsync();
+            // Find current users full name to link it with the bill
+            // Since email never changes, it is safest to use it as the index to determine this
+            Users = await _context.UserModel.ToListAsync();
+            foreach (var user in Users)
+            {
+                if (user.Email == User.Identity.Name)
+                {
+                    DriverName = user.Name;
+                    break;
+                }
+            }
             // loop through each freight, set needed values
             foreach (var item in FreightBill)
             {
-                Id.Add(item.ID);
-                BillName.Add(item.Customer);
-                StartDate.Add(item.ScheduledDate);
-                //DueDate.Add(item.DueDate);
-                Status.Add(item.Status);
+                // checks if logged in user is assigned this freight
+                if (item.Driver == DriverName)
+                {
+                    Id.Add(item.ID);
+                    BillName.Add(item.Customer);
+                    StartDate.Add(item.ScheduledDate);
+                    Status.Add(item.Status);
+                }
             }
         }
     }
