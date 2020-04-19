6d85e5d526147bbab15e86de9375ff784c86bc5e
Peter Menchu <menchupeter2@gmail.com>
2020-04-19 08:23:23 -0500

    I noticed that if any more tabs get added, the nav bar will break if the page is minimized to right before it turns into the hamburger menu, so I went ahead and changed the users tab to employees and added the timesheet links there to avoid issues. Not sure how to go about the actual content for the timesheets yet, but added the related pages since they are up next.

---
+++ GlrTransportInc/Pages/Timesheets/AllTimesheets.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GlrTransportInc.Data;
+using GlrTransportInc.Models;
+
+namespace GlrTransportInc.Pages.Timesheets
+{
+    public class AllTimesheetsModel : PageModel
+    {
+        
+    }
+}
