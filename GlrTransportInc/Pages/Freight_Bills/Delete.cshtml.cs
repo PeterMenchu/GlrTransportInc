dab9aace82900cc8018cbeabc12630fc11e22636
soyrodrigo01 <56319147+soyrodrigo01@users.noreply.github.com>
2020-04-20 00:47:12 -0500

    Files now delete from the server when removing a freight bill.
    As far as the other part: "or adding a different permit", this is already taken care of by the file name normalization (all permits will have the id as the file name), when another permit is uploaded, it just takes the name of the old one, replacing it.

--- GlrTransportInc/Pages/Freight_Bills/Delete.cshtml.cs
+++ GlrTransportInc/Pages/Freight_Bills/Delete.cshtml.cs
@@ -7,16 +7,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GlrTransportInc.Data;
 using GlrTransportInc.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace GlrTransportInc
 {
     public class DeleteModel : PageModel
     {
         private readonly GlrTransportInc.Data.ApplicationDbContext _context;
-
-        public DeleteModel(GlrTransportInc.Data.ApplicationDbContext context)
+        private IWebHostEnvironment _environment;
+        public DeleteModel(GlrTransportInc.Data.ApplicationDbContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
 
         [BindProperty]
@@ -61,6 +64,11 @@ namespace GlrTransportInc
 
             if (FreightBill != null)
             {
+                if (FreightBill.Permit != null)
+                {
+                    var file = Path.Combine(_environment.ContentRootPath, "wwwroot/permits", FreightBill.Permit.Substring(8));
+                    System.IO.File.Delete(file);
+                }
                 _context.FreightBill.Remove(FreightBill);
                 await _context.SaveChangesAsync();
             }
