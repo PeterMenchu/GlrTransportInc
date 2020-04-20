64a87cf3b6eb92a811e7d9ce5b7e1b0c5369a8dc
soyrodrigo01 <56319147+soyrodrigo01@users.noreply.github.com>
2020-04-20 17:19:48 -0500

    Added javascript to restrict the file types. It wasn't restricting them on mobile. Will test.

--- GlrTransportInc/wwwroot/js/site.js
+++ GlrTransportInc/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var file = document.getElementById('onlypdf');
+
+file.onchange = function (e) {
+    var ext = this.value.match(/\.([^\.]+)$/)[1];
+    switch (ext) {
+        case 'pdf':
+            break;
+        default:
+            alert('Not allowed');
+            this.value = '';
+    }
+};
