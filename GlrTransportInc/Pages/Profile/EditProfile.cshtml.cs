c5deaa92997c340a547ab6cdd74d70b9722b88be
Peter Menchu <menchupeter2@gmail.com>
2020-04-22 02:57:08 -0500

    Finished fixing edit name, added if statement to separate the phone# and name updates.

--- GlrTransportInc/Pages/Profile/EditProfile.cshtml.cs
+++ GlrTransportInc/Pages/Profile/EditProfile.cshtml.cs
@@ -15,11 +15,12 @@ namespace GlrTransportInc.Pages.Profile
 {
     public class EditProfileModel : PageModel
     {
+        // these are used to load data relevant to this page
         private readonly UserManager<UserModel> _userManager;
         private readonly SignInManager<UserModel> _signInManager;
-
-        public string Name;
-        //private readonly RoleManager<UserModel> _roleManager;
+        // setter value for 
+        private string _name;
+        
         public EditProfileModel(
             UserManager<UserModel> userManager,
             SignInManager<UserModel> signInManager
@@ -29,69 +30,63 @@ namespace GlrTransportInc.Pages.Profile
             _userManager = userManager;
             _signInManager = signInManager;
         }
-
-        public static string Username { get; set; }
+        // status message declaration
         [TempData]
         public string StatusMessage { get; set; }
-
+        // declare instance of input model for storing input data, defined below
         [BindProperty]
         public InputModel Input { get; set; }
-
+        // InputModel definition, defines the phone# and name variables that store user input which also 
+        // display current values as placeholders
         public class InputModel
         {
             [Phone]
             [Display(Name = "Phone number:")]
             public string PhoneNumber { get; set; }
             public string Fullname { get; set; }
-            
-            
-            
         }
-
+        // function for setting current values for display
         private async Task LoadAsync(UserModel user)
         {
-            var userName = await _userManager.GetUserNameAsync(user);
+            // handles setting current values
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            //var position = await _roleManager.GetRoleNameAsync(user);
-            Username = userName;
-            //UserPosition = position;
-
+            // sets placeholder values
             Input = new InputModel
             {
                 PhoneNumber = phoneNumber,
-                Fullname = Name
+                Fullname = _name
             };
-            
-            
         }
-
+        // function for loading user table data
         public async Task<IActionResult> OnGetAsync()
         {
+            // load user data context 
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
-            Name = user.Name;
+            // to avoid problems with static/non-static variables, using Name as setter for Fullname/input variable
+            _name = user.Name;
+            // calls function that handles setting the input/placeholders
             await LoadAsync(user);
-
             return Page();
         }
-
+        // once save is clicked perform Post
         public async Task<IActionResult> OnPostAsync()
         {
+            // ensure user info is current via reloading data
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
-
             if (!ModelState.IsValid)
             {
                 await LoadAsync(user);
                 return Page();
             }
-
+            // check input with current data, first for phonenumber
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             if (Input.PhoneNumber != phoneNumber)
             {
@@ -102,9 +97,12 @@ namespace GlrTransportInc.Pages.Profile
                     throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
                 }
             }
-            
-            int set = addName(User.Identity.Name, Input.Fullname, user.Name);
-            
+            // then check for edited name
+            if (Input.Fullname != user.Name && Input.Fullname != null) 
+            {
+                int set = addName(User.Identity.Name, Input.Fullname, user.Name);
+            }
+            // finish update
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
