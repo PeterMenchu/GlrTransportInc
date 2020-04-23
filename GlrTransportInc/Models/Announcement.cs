ef25ee416a5f03c5e062d83aa024ddf0085ed76e
soyrodrigo01 <56319147+soyrodrigo01@users.noreply.github.com>
2020-04-22 20:14:57 -0500

    Make announcements require both a title and a body

--- GlrTransportInc/Models/Announcement.cs
+++ GlrTransportInc/Models/Announcement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,12 @@ namespace GlrTransportInc.Models
     public class Announcement
     {
         public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Author { get; set; }
         public DateTime DatePosted { get; set; }
+        [Required]
         public string Post { get; set; }
     }
 }
