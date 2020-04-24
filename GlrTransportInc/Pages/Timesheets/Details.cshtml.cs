8cd1ca6cf2941da27eec53a28e5391614915b7b4
Peter Menchu <menchupeter2@gmail.com>
2020-04-24 06:43:46 -0500

    Added unassigned drivers for bills, changed namespace paths for timesheets to avoid compilation errors

---
+++ GlrTransportInc/Pages/Timesheets/Details.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GlrTransportInc.Data;
+using GlrTransportInc.Models;
+
+namespace GlrTransportInc.Pages.Timesheets
+{
+    public class DetailsModel3 : PageModel
+    {
+        private readonly GlrTransportInc.Data.ApplicationDbContext _context;
+
+        public DetailsModel3(GlrTransportInc.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Timesheet Timesheet { get; set; }
+        public IList<UserModel> Users { get; set; }
+        public static string Name;
+        public static string Position;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Timesheet = await _context.Timesheet.FirstOrDefaultAsync(m => m.ID == id);
+            Users = await _context.UserModel.ToListAsync();
+            foreach (var item in Users)
+            {
+                if ((item.Email) == User.Identity.Name)
+                {
+                    Name = item.Name;
+                    Position = item.Position;
+                }
+            }
+
+            if (Timesheet == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ GlrTransportInc/Pages/Timesheets/MyIndex.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GlrTransportInc.Data;
+using GlrTransportInc.Models;
+
+namespace GlrTransportInc.Pages.Timesheets
+{
+    public class MyIndexModel : PageModel
+    {
+        private readonly GlrTransportInc.Data.ApplicationDbContext _context;
+
+        public MyIndexModel(GlrTransportInc.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Timesheet> Timesheet { get; set; }
+        public IList<UserModel> Users { get; set; }
+        public static string Name;
+        public static string Position;
+        public async Task OnGetAsync()
+        {
+            Timesheet = await _context.Timesheet.OrderByDescending(a => a.ID).ToListAsync();
+            Users = await _context.UserModel.ToListAsync();
+            foreach (var item in Users)
+            {
+                if ((item.Email) == User.Identity.Name)
+                {
+                    Name = item.Name;
+                    Position = item.Position;
+                }
+            }
+        }
+    }
+}
