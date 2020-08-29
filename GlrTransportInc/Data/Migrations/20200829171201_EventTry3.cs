43a52398a6641299d6cfe3aaf024a9115990ff8f
Peter Menchu <menchupeter2@gmail.com>
2020-08-29 12:13:08 -0500

    Events try 3

---
+++ GlrTransportInc/Data/Migrations/20200829171201_EventTry3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GlrTransportInc.Data.Migrations
+{
+    public partial class EventTry3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ GlrTransportInc/Models/EventListing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlrTransportInc.Models
+{
+    public class EventListing
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? EventDate { get; set; }
+
+        public string Post { get; set; }
+    }
+}
