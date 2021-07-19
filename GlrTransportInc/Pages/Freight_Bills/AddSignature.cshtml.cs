e3bca60d8b03b860974e5471e1464a58e68171e8
Peter Menchu <menchupeter2@gmail.com>
2021-07-19 02:47:47 -0500

    Part 1 of signature update, drawing and clearing the canvas added.

---
+++ GlrTransportInc/Pages/Freight_Bills/AddSignature.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GlrTransportInc.Pages.Freight_Bills
+{
+    public class AddSignature : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
