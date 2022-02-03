4d498f4dbc2f831ddc853388a497a9b3a71dfec8
Peter Menchu <menchupeter2@gmail.com>
2022-02-03 04:08:42 -0600

    Fixing mobile styling on lists, bill -> signature button

--- GlrTransportInc/Pages/Freight_Bills/Files.cshtml.cs
+++ GlrTransportInc/Pages/Freight_Bills/Files.cshtml.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using GlrTransportInc.Data;
 using GlrTransportInc.Models;
